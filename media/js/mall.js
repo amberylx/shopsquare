b4878043e4ae645dd3df9d6b72e33accb5e42447
stephanie lee <amberyl.x@gmail.com>
2012-11-02 20:33:21 -0700

    add sortable plugin, make header thinner

--- media/js/mall.js
+++ media/js/mall.js
@@ -1,4 +1,16 @@
 $(function() {
+    $("#mall").sortable();
+    $("#floor1").sortable({
+	cursor:'move',
+	cursorAt: {left:5},
+	containment:'parent',
+	forcePlaceholderSize: true,
+	// placeholder:'sortable-placeholder',
+	//grid: [100,150]
+	helper:'clone',
+	opacity:0.5,
+	revert:true
+    });
     $(".addstorecontainer").on("click", function() {
 	$(".addstoreformcontainer").slideDown(500);
     });
