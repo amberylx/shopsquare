f962f8c15326f402159a2df40d9884663c67dba8
stephaniel <stephaniel@angieslist.com>
2012-11-04 20:32:21 -0800

    miscellaneous checkins

--- urls.py
+++ urls.py
@@ -5,12 +5,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('mall.views',
     url(r'^mall/(\d*)/$', 'mall', {}, name="mall"),
+    url(r'^add_to_wishlist/$', 'add_to_wishlist', {}, name='add_to_wishlist'),
     url(r'^add_store/$', 'add_store', {}, name="add_store"),
     url(r'^remove_store/$', 'remove_store', {}, name="remove_store"),
     url(r'^move_store/$', 'move_store', {}, name="move_store"),
     url(r'^edit_floorplan/(\d*)/$', 'edit_floorplan', {}, name="edit_floorplan"),
     url(r'^register/$', 'register', {}, name="register"),
-    url(r'^profile/$', 'profile', {}, name="profile"),
+    url(r'^profile/(\d*)/$', 'profile', {}, name="profile"),
+    url(r'^profile/(\d*)/wishlist/$', 'wishlist', {}, name="wishlist"),
+    url(r'^about/$', 'about', {}, name="about"),
     url(r'^login/$', 'login', name="login"),
     url(r'^logout/$', 'logout', name="logout"),
 )
