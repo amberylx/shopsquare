d6bf8f6ce7794b3c7428e928bba61a77e21508ac
stephaniel <stephaniel@angieslist.com>
2012-11-17 20:23:36 -0800

    fix imports, add utility to parse primary domain from url

--- utils/urlutils.py
+++ utils/urlutils.py
@@ -1,6 +1,6 @@
 from time import time
-from utils import slugify
-import MyGlobals
+from urlparse import urlparse
+import slugify
 
 def getStoreImageFilename(storename, storeid):
     return "%s-%s.jpg" % (slugify.slugify(storename), storeid)
@@ -11,6 +11,17 @@ def getScrapedImageFilename():
 def getWishlistImageFilename(wlid):
     return "%s.jpg" % wlid
 
+def getDomainFromUrl(url):
+    split_domain = urlparse(url)[1].split('.')
+    if len(split_domain) == 2:
+        domain = split_domain[0]
+    elif len(split_domain) == 3:
+        domain = split_domain[1]
+    elif len(split_domain) == 1:
+        domain = split_domain[0]
+    return domain
+
 if __name__ == "__main__":
-    pass
+    print getDomainFromUrl('http://imm-living.com/index.php?p=product&cat_id=8')
+    print getDomainFromUrl('http://www.urbanoutfitters.com/urban/catalog/productdetail.jsp?id=24033169&parentid=W_APP_DRESSES')
         
