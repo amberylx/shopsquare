2a27b7da5a6679441ca9993b02793950742c8549
stephaniel <stephaniel@angieslist.com>
2012-11-19 23:53:28 -0800

    add exception handling to system utilities

--- utils/sysutils.py
+++ utils/sysutils.py
@@ -2,10 +2,16 @@ import subprocess
 
 def move_file(movefrom, moveto):
     print "moving file %s --> %s" % (movefrom, moveto)
-    subprocess.Popen(['mv %s %s' % (movefrom, moveto)], shell=True)
+    try:
+        subprocess.Popen(['mv %s %s' % (movefrom, moveto)], shell=True)
+    except Exception, e:
+        print "[ERROR] unable to move file: %s, %s" % (movefrom, moveto)
     return
 
 def delete_file(filepath):
     print "remove file %s" % filepath
-    subprocess.Popen(['rm %s'%imgpath], shell=True)
+    try:
+        subprocess.Popen(['rm %s'%imgpath], shell=True)
+    except Exception, e:
+        print "[ERROR] unable to remove file: %s" % filepath
     return
